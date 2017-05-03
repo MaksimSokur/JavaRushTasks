b1bf6f837804a354a6c5ccf4813bba1a7b178ff4
MaksimSokur <maksim.sokur90@gmail.com>
2017-05-03 13:28:32 +0300

    My Java Rush

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task18.task1827;
+
+/* 
+Прайсы
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        FileInputStream inputStream = new FileInputStream(fileName);
+    }
+}
